c116e016e7797c003b2c58b10cdd3c0e924319b7
John Mavrick <john@kortex.co>
2024-05-09 20:23:16 -0700

    Remove page title left bar

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -83,7 +83,6 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     // Component.Darkmode(),
   ],
