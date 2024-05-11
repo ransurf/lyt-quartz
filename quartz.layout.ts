2ed2604aa2c0ae0996f73ba4d58412687bd172bf
John Mavrick <john@kortex.co>
2024-05-10 19:01:34 -0700

    recent ideas are turned to recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,11 +50,11 @@ export const defaultContentPageLayout: PageLayout = {
     ),
   Component.DesktopOnly(
     Component.RecentNotes({
-      title: "Recent Ideas",
+      title: "Recent Notes",
       showDates: true,
       limit: 2,
-      filter: (f) => f.slug!.startsWith("ideas/"),
-      linkToMore: "ideas/" as SimpleSlug,
+      filter: (f) => f.slug!.startsWith("notes/"),
+      linkToMore: "notes/" as SimpleSlug,
     }),
   ),
   Component.DesktopOnly(
