942af17c522cd4ae264ef81d0d1e765e5b0d60bf
John Mavrick <john@kortex.co>
2024-05-23 23:57:23 -0700

    make about author text grayer

--- quartz/components/AboutAuthor.tsx
+++ quartz/components/AboutAuthor.tsx
@@ -45,7 +45,7 @@ export default ((userOpts?: Partial<Options>) => {
     return (
       <div class={classNames(displayClass, "about-author")}>
         <h3 className="component-title">{i18n(cfg.locale).components.aboutAuthor.title}</h3>
-        <p>{i18n(cfg.locale).components.aboutAuthor.description}</p>
+        <p className="about-author-text">{i18n(cfg.locale).components.aboutAuthor.description}</p>
         {/* {opts.linkToMore && remaining > 0 && (
           <p>
             <a href={resolveRelative(fileData.slug!, opts.linkToMore)}>
