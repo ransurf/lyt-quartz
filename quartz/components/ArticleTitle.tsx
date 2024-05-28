7a63c36cff17ba7c41b5918397a6369d7286cc98
John Mavrick <john@kortex.co>
2024-05-28 14:31:51 -0700

    disableTitle frontmatter compatibility

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,7 +3,8 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
-  if (title) {
+  const isDisabled = fileData.frontmatter?.disableTitle
+  if (title && !isDisabled) {
     return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
     return null
