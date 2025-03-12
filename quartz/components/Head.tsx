30f8e5e40339dcb442de608cc3518a2ae82d944a
John Mavrick <john@kortex.co>
2025-03-12 17:42:27 -0600

    prefix tab name with LYT Blog, replace index with it

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -6,7 +6,8 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 
 export default (() => {
   const Head: QuartzComponent = ({ cfg, fileData, externalResources }: QuartzComponentProps) => {
-    const title = fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
+    const pageName = fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
+    const title = pageName === "index" ? "LYT Blog" : `LYT Blog | ${pageName}`
     const description =
       fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description
     const { css, js } = externalResources
