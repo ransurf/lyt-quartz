6b37ae595e7ee71caefb2b501f2b570428f99fed
John Mavrick <john@kortex.co>
2025-03-13 16:41:44 -0600

    remove team folder

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "_team"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "local",
